f068448103ab71fe79015d71e11aa761d5f1c137
xiaoxian521 <1923740402@qq.com>
2023-02-13 19:13:14 +0800

    fix: `version` is declared here

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,9 @@ export type Version = 2 | 3;
 
 export interface StorageOpts {
   /** vue版本 可选2、3 默认3 */
-  version: Version;
+  version?: Version;
   /** 命名空间 默认  `rs-` */
-  nameSpace: string;
+  nameSpace?: string;
   /** 需要存储的响应式对象 */
   memory: object;
 }
